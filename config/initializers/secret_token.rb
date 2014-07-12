445634b2958259814848f73d5c5b758a6d7f0ce1
moultonjr <moultonjust@gmail.com>
2014-07-12 19:03:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClockInClockOut::Application.config.secret_key_base = '9257056d57e5296cd20e812f056d3b6822357eee6d9eca055a7757725a30bf202a143869e26465a3bdaaffaff6e1f8c4d2ade402b4ab22dc7468f8aef6b68505'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ClockInClockOut::Application.config.session_store :cookie_store, key: '_clock_in_clock_out_session'
